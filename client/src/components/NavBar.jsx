673545e018c048cfbda90f0f8a04541e004e8698
Lucas Gong <lucas.w.gong@gmail.com>
2021-09-30 01:30:04 -0700

    Create - Drawer w/o link

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,12 @@
+import { useState } from "react";
 import { AppBar, Box, Toolbar, IconButton, Typography } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import { SwipeableDrawer, List, ListItem, ListItemIcon, ListItemText, Divider } from "@mui/material";
+import HomeIcon from '@mui/icons-material/Home';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import QuizIcon from '@mui/icons-material/Quiz';
+import LoginIcon from '@mui/icons-material/Login';
+import CreateIcon from '@mui/icons-material/Create';
+import MenuIcon from '@mui/icons-material/Menu';
 import NavBarBtn from "./NavBarBtn";
 import { useMediaQuery } from "react-responsive"
 
@@ -9,31 +16,76 @@ const NavBar = () => {
     });
     const displayMode = mobileView ? "block" : "none"
 
+    const [drawer, setDrawer] = useState(false);
+    const toggleDrawer = (isOpen) => (e) => {
+        setDrawer(isOpen);
+    };
+
+    const list = (
+        <Box
+            sx={{ width: 250 }}
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+            >
+            <List>
+                {['Home', 'Admin', 'Student'].map((text, index) => (
+                <ListItem button key={text}>
+                    <ListItemIcon>
+                    {index % 3 === 0 ? <HomeIcon /> : index === 1 ? <AdminPanelSettingsIcon /> : <QuizIcon/>}
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['SignIn', 'SignUp'].map((text, index) => (
+                <ListItem button key={text}>
+                    <ListItemIcon>
+                    {index % 2 === 0 ? <LoginIcon /> : <CreateIcon />}
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
+
     return (
     <Box sx={{ flexGrow: 1}}>
-    <AppBar position="static">
-        <Toolbar>
-            <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                sx={{ mr: 1 }}
-            >
-                <MenuIcon fontSize="large"/>
-            </IconButton>
-            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-                Quiz CRUD App
-            </Typography>
-            <span style={{ display: displayMode }}>
-                <NavBarBtn to="/" value="Home"/>
-                <NavBarBtn to="/admin" value="Admin"/>
-                <NavBarBtn to="/student" value="Student"/>
-                <NavBarBtn to="/signin" value="SignIn"/>
-                <NavBarBtn to="/signup" value="SignUp"/>
-            </span>
-        </Toolbar>
-    </AppBar>
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    onClick={toggleDrawer(true)}
+                    sx={{ mr: 1 }}
+                >
+                    <MenuIcon fontSize="large"/>
+                </IconButton>
+                <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+                    Quiz CRUD App
+                </Typography>
+                <span style={{ display: displayMode }}>
+                    <NavBarBtn to="/" value="Home"/>
+                    <NavBarBtn to="/admin" value="Admin"/>
+                    <NavBarBtn to="/student" value="Student"/>
+                    <NavBarBtn to="/signin" value="SignIn"/>
+                    <NavBarBtn to="/signup" value="SignUp"/>
+                </span>
+            </Toolbar>
+        </AppBar>
+        <SwipeableDrawer
+            anchor="left"
+            open={drawer}
+            onClose={toggleDrawer(false)}
+            onOpen={toggleDrawer(true)}
+        >
+            {list}
+        </SwipeableDrawer>
     </Box>
     );
 };
